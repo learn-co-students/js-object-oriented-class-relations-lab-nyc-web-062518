2b9e2e135c5849d1e1f78324788a7567d50d80ea
jaredianmills <jaredianmills@gmail.com>
2018-08-08 12:52:48 -0400

    Done

---
+++ index.js
@@ -0,0 +1,56 @@
+let driverId = 0
+
+const store = {drivers: [], passengers: [], trips: []}
+
+class Driver {
+  constructor(name) {
+    this.name = name
+    this.id = ++driverId
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter((trip) => trip.driverId === this.id)
+  }
+
+  passengers() {
+    return this.trips().map((trip) => trip.passenger())
+  }
+}
+
+let passengerId = 0
+
+class Passenger {
+  constructor(name) {
+    this.name = name
+    this.id = ++passengerId
+    store.passengers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter((trip) => trip.passengerId === this.id)
+  }
+
+  drivers() {
+    return this.trips().map((trip) => trip.driver())
+  }
+}
+
+let tripId = 0
+
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    this.id = ++tripId
+    store.trips.push(this)
+  }
+
+  driver() {
+    return store.drivers.find((driver) => this.driverId === driver.id)
+  }
+
+  passenger() {
+    return store.passengers.find((passenger) => this.passengerId === passenger.id)
+  }
+}
