73c73be5ea370bf71728347fe479d4038d6d6e03
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-08-08 20:02:55 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,69 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let driverId = 0
+
+class Driver {
+
+  constructor(name) {
+    this.name = name
+    this.id = ++driverId
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(trip => trip.driverId === this.id)
+  }
+
+  passengers() {
+    return this.trips().map(trip => trip.passenger())
+  }
+
+}
+
+let passengerId = 0
+
+class Passenger {
+
+  constructor(name) {
+    this.name = name
+    this.id = ++passengerId
+    store.passengers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(trip => trip.passengerId === this.id)
+  }
+
+  drivers() {
+    return this.trips().map(trip => trip.driver())
+  }
+
+}
+
+let tripId = 0
+
+class Trip {
+
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    store.trips.push(this)
+  }
+
+  setDriver(driver) {
+    this.driverId = driver.id
+  }
+
+  setPassenger(passenger) {
+    this.passengerId = passenger.id
+  }
+
+  driver() {
+    return store.drivers.find(driver => this.driverId === driver.id)
+  }
+
+  passenger() {
+    return store.passengers.find(passenger => this.passengerId === passenger.id)
+  }
+}
